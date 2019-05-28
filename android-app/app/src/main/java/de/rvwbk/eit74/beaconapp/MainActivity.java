f51da2f33804b6a7a3bc8d7bb5aab00ada132747
MarkMarkMark <markdueker@googlemail.com>
2019-05-28 10:31:19 +0200

    Main Activity added

---
+++ android-app/app/src/main/java/de/rvwbk/eit74/beaconapp/MainActivity.java
@@ -0,0 +1,13 @@
+package de.rvwbk.eit74.beaconapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
