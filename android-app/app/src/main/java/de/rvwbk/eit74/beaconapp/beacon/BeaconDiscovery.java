65367808f7b1d9a9bdfcdcf67de165a49ffaeed8
Dustin <dustin.simon@8mylez.com>
2019-06-17 08:49:20 +0200

    sending broadcast when beacon was found

--- android-app/app/src/main/java/de/rvwbk/eit74/beaconapp/beacon/BeaconDiscovery.java
+++ android-app/app/src/main/java/de/rvwbk/eit74/beaconapp/beacon/BeaconDiscovery.java
@@ -2,6 +2,8 @@ package de.rvwbk.eit74.beaconapp.beacon;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
 
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
@@ -11,10 +13,52 @@ import com.estimote.coresdk.service.BeaconManager;
 
 import java.util.List;
 
+/**
+ *
+ *  Here is some code you can use in your activity to catch the event when a beacon is found.
+ *
+ *  The intent contains the id of the beacon which was found.
+ *
+ *
+    BroadcastReceiver connectionLostReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(!intent.getAction().equals(BeaconDiscovery.ACTION_BEACON_FOUND)) {
+                return;
+            }
+
+            String beaconID = intent.getStringExtra("beaconID");
+
+            if(beaconID.equals(BeaconDiscovery.BEACON_DARK_BLUE_ID)) {
+
+                //It's the dark blue beacon
+
+            } else if(beaconID.equals(BeaconDiscovery.BEACON_MINT_ID)) {
+
+                //It's the mint beacon
+
+            }
+
+            // Do your thing here
+
+        }
+    };
+
+    LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(connectionLostReceiver, new IntentFilter(BeaconDiscovery.ACTION_BEACON_FOUND));
+
+
+ */
+
 public class BeaconDiscovery {
 
     public static final int[] BEACON_DARK_BLUE = {47140, 49207};
     public static final int[] BEACON_MINT = {62040, 5975};
+
+    public static final String BEACON_DARK_BLUE_ID = "47140-49207";
+    public static final String BEACON_MINT_ID = "62040-5975";
+
+    public static final String ACTION_BEACON_FOUND = "de.rvwbk.eit74.beaconapp.beacon.BeaconDiscovery.ACTION_BEACON_FOUND";
 
     private static BeaconDiscovery ourInstance = null;
 
@@ -73,12 +117,27 @@ public class BeaconDiscovery {
                     String minor = Integer.toString(b.getMinor());
 
                     Toast.makeText(activity, major + "\n" + minor, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(activity, uuid, Toast.LENGTH_LONG).show();
+
+                    sendBroadcast(activity, major + "-" + minor);
                 }
 
                 beaconManager.stopRanging(beaconRegion);
+
             }
         });
 
 
     }
+
+    private void sendBroadcast(Activity activity, String id) {
+        Intent intent = new Intent();
+
+        intent.putExtra("beaconID", id);
+        intent.setAction(ACTION_BEACON_FOUND);
+
+        LocalBroadcastManager.getInstance(activity.getApplicationContext()).sendBroadcast(intent);
+    }
+
+
 }
